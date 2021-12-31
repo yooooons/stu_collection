9bc7c6029427d73acb8bf44fc99af5fbe1658caa
YS <yooooonnf@gmail.com>
2021-12-31 21:47:41 +0900

    third commit

---
+++ src/list/ArrayListMethod.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListMethod {
+    public static void main(String[] args) {
+        List<Integer> aList1 =new ArrayList<>();
+
+        aList1.add(3);
+        aList1.add(4);
+        aList1.add(5);
+        System.out.println(aList1.toString());
+    }
+}
